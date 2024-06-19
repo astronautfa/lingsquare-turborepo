ec69143ea51c7bd9247b58fb83e6f4d0ba545ecc
astronautfa <145324740+astronautfa@users.noreply.github.com>
2024-06-19 15:41:35 +0330

    todos added

--- apps/dashboard/app/page.tsx
+++ apps/dashboard/app/page.tsx
@@ -12,3 +12,41 @@ export default function Example() {
     </div>
   )
 }
+
+// TODO : find better style for the sidebar collapse button
+// TODO : fix animation of account switcher
+// TODO : sort out the icons
+
+// TODO : add ripple effect to buttons
+// https://www.npmjs.com/package/tailwindcss-ripple
+// https://codepen.io/vituja1/pen/oNWzNwq
+
+// TODO : Create notification component
+
+// TODO : add breadcrumb
+
+// TODO : add language flag icons
+// TODO : add known language
+
+// TODO : fix background colors not consistent
+// TODO : add not defined page
+
+// TODO : add support component
+// TODO : open sidebar and close tooltip
+
+// TODO : fullscreen application button
+
+// TODO : fix mode animation
+
+// TODO : explore cmdk with generative ui
+// TODO : add the player
+
+// TODO : sort out login
+
+// TODO : interface language
+// TODO : language settings
+
+// TODO : check how dub.co modals are managed
+// TODO : check the infintestune login modals
+
+// TODO : explore zustand vs valtio
