c0ebcecd5752ed990e49199523b4b1849d49f290
astronautfa <145324740+astronautfa@users.noreply.github.com>
2024-06-21 23:10:53 +0330

    modal intercepting route cleaned up

--- apps/dashboard/app/(app)/layout.tsx
+++ apps/dashboard/app/(app)/layout.tsx
@@ -1,20 +1,13 @@
 import React from 'react'
-import ModalOrNot from '@/components/@modal/ModalOrNot';
-import ModalProvider from '@/components/@modal/ModalProvider';
 import { SidebarLayout } from '@/components/sidebar-layout';
 
 const AppLayout = ({
     children,
-    modal
 }: Readonly<{
     children: React.ReactNode;
-    modal: React.ReactNode;
 }>) => {
     return (
         <SidebarLayout>
-            <ModalProvider>
-                <ModalOrNot>{modal}</ModalOrNot>
-            </ModalProvider>
             {children}
         </SidebarLayout>
     )
