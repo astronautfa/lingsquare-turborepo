33ef98c5a53a24c3586363fd2f6bdf4b4f1d5552
astronautfa <145324740+astronautfa@users.noreply.github.com>
2024-08-03 13:42:58 +0330

    ui color changes

--- apps/app/src/components/mode-toggle.tsx
+++ apps/app/src/components/mode-toggle.tsx
@@ -28,8 +28,8 @@ export function ModeToggle({ collapsed }: { collapsed: boolean }) {
           onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
         >
           <div className='w-5 relative mb-5'>
-            <SunIcon className="absolute w-[1.3rem] h-[1.3rem] rotate-90 scale-0 transition-transform ease-in-out duration-500 dark:rotate-0 dark:scale-100" />
-            <MoonIcon className="absolute w-[1.3rem] h-[1.3rem] rotate-0 scale-1000 transition-transform ease-in-out duration-500 dark:-rotate-90 dark:scale-0" />
+            <SunIcon className="absolute w-[1.3rem] text-zinc-400 h-[1.3rem] rotate-90 scale-0 transition-transform ease-in-out duration-500 dark:rotate-0 dark:scale-100" />
+            <MoonIcon className="absolute w-[1.3rem] text-zinc-500 h-[1.3rem] rotate-0 scale-1000 transition-transform ease-in-out duration-500 dark:-rotate-90 dark:scale-0" />
           </div>
           <div className="ml-3">
             <HidableSidebarLabel collapsed={collapsed}>
